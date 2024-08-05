987f81fcfdc5f122b15cbc5ced1b0cf66485b579
Austin K <austink@Austins-MacBook-Pro.local>
2024-08-05 13:25:00 -0400

    Added adult_passengers method

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -1,3 +1,10 @@
 class Airline < ApplicationRecord
    has_many :flights
+
+   def adult_passengers
+      Passenger.joins(flights: :airline)
+               .where("airlines.id = ?", id)
+               .where("passengers.age >= ?", 18)
+               .distinct
+   end
 end
